33f450221fafce85906ae0676add02bee585f1f3
Camila <coranteshuezo@bostonk12.org>
2024-07-12 15:02:30 -0400

    Initial Commit

---
+++ githubtutorial/githubtutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtutorialApp.swift
+//  githubtutorial
+//
+//  Created by Scholar on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct githubtutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
